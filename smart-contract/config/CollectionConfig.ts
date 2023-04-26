dac70e9cfef67f545ab9d263cb723a633c1459a2
FreeVoicesDev <heel.jaunts.0s@icloud.com>
2023-04-26 19:12:01 +0100

    commit 2

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -9,24 +9,24 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
-  maxSupply: 10000,
+  contractName: 'FVTestingNftV2',
+  tokenName: 'Fv Testing V2',
+  tokenSymbol: 'FVT',
+  hiddenMetadataUri: 'ipfs://QmcfFTFuVn2fe6AQT17xbjwd6yWJnALjUzpaHGDBWMthNN/hidden.json',
+  maxSupply: 100,
   whitelistSale: {
-    price: 0.05,
+    price: 0.01,
     maxMintAmountPerTx: 1,
   },
   preSale: {
-    price: 0.07,
+    price: 0.02,
     maxMintAmountPerTx: 2,
   },
   publicSale: {
-    price: 0.09,
+    price: 0.05,
     maxMintAmountPerTx: 5,
   },
-  contractAddress: null,
+  contractAddress: "0x17219828CdEA6e9870076f74eAFbFD36e9F5A110",
   marketplaceIdentifier: 'my-nft-token',
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
